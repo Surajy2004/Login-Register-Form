84b8f05a94a4848ff5ede75c4bfb227a4974b971
suraj <ss7380425524@gmail.com>
2023-04-17 02:18:56 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function logx() {
+
+    var log = document.querySelector("#log")
+    var reg = document.querySelector("#reg")
+    log.style.display = "block"
+    reg.style.display = "none"
+}
+function regx() {
+
+    var reg = document.querySelector("#reg")
+    var log = document.querySelector("#log")
+    reg.style.display = "block"
+    log.style.display = "none"
+
+}
